61dea90c82705115dc94cbda7415221d65cc0133
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-09-22 23:35:24 -0400

    added title, sections, license info, Questions, and Table of Contents

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+
+    // title of the project
+
+    {
+        name: "title",
+        message: "what is the title of your project?"
+
+    },
+
+    // sections
+
+    //Description
+
+    {
+        name: "",
+        message:
+
+    }
+
+    // Installation
+
+
+    // Usage
+
+    // Contribution
+
+    // Tests
+
+    // license Information
+
+    // Questions : Github username added with a link to GitHub profile, 
+    // Questions: Email address added with instruction on how to reach me with additional questions, 
+
+    // Table of Contents: Taken to the corresponding section of the README
+
+
+
+])
