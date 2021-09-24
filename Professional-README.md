e48af22f89e7310d2abd1977ad8a741eb2d2b9e3
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-09-24 17:21:52 -0400

    tested on terminal

---
+++ Professional-README.md
@@ -0,0 +1,45 @@
+ 
+===
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/license/MIT)
+
+Description
+---
+Creating a README file by using a command-line application 
+
+Table of Contents
+---
+- [Description] (#description)
+- [Installation] (#installation)
+- [Usage] (#usage)
+- [License] (#license)
+- [Contributing] (#contributing)
+- [Tests] (#tests)
+
+
+Installation
+---
+User will need to clone the README repo to their local computer to start using it. Inquirer, the npm package, needs to be downloaded and a index.js file created to start the project
+
+Usage
+---
+This README generator will enable user to create a professional README file to use with his project
+
+License
+---
+[MIT License](https://opensource.org/license/MIT)
+
+Contributing
+---
+Follow gitHub guidelines, adhere to coding standards
+
+Tests
+---
+none
+
+Questions
+---
+
+-What is your GitHub username?
+	- It is:bkhanal87
+
--- index.js
+++ index.js
@@ -4,51 +4,35 @@ const { title } = require("process");
 
 inquirer
   .prompt([
-    // title of the project
-
     {
       name: "userTitle",
       message: "Enter title of the project.",
     },
-
-    // sections
-
-    // Description
 
     {
       name: "userDescription",
       message: "Enter description of the project.",
     },
 
-    // Installation
-
     {
       name: "installation",
       message: "Provide instructions on how to install (if applicable).",
     },
-
-    // Usage Information
 
     {
       name: "usage",
       message: "Enter usage information for your project.",
     },
 
-    // Contribution
-
     {
       name: "contributing",
       message: "Inform how other developers can contribute to your project, what the guidelines are, limitations, any rules, etc.",
     },
 
-    // Tests
-
     {
       name: "tests",
       message: "Provide information on how to run the project",
     },
-
-    // license Information
 
     {
       name: "license",
@@ -63,27 +47,11 @@ inquirer
       type: "list"
     },
 
-    // {
-    //   name: "license",
-    //   choices: [
-    //     "ISC",
-    //     "MIT",
-    //     "Apache License 2.0",
-    //     "The Unlicense",
-    //     "Mozilla Public License 2.0",
-    //   ],
-    //   type: "list",
-    // },
-
-    // Questions : Github username added with a link to GitHub profile,
-
     {
       name: "gitHubuserName",
-      message: "Enter your gitHub username"
+      message: "Enter your gitHub username",
     
     },
-
-    // Questions: Email address added with instruction on how to reach me with additional questions,
 
     {
       name: "userEmail",
@@ -107,15 +75,10 @@ inquirer
     let questionContent = (gitHubuserName || userEmail) && (gitHubuserName.length + userEmail.length);
 
     let text = `${userTitle&&userTitle.length?title + "\n===\n":""}
-
 ${license&&license.length?licenser.getBadge(license)+ "\n":""}
 ${userDescription&&userDescription.length?"Description\n---\n"+userDescription:""}
 
-
-
-
-
-// Table of Contents: Taken to the corresponding section of the README
+__TOC__
 
 ${installation&&installation.length?"Installation\n---\n"+installation:""}
 
@@ -125,29 +88,28 @@ ${license&&license.length?"License\n---\n"+licenser.getText(license):""}
 
 ${contributing&&contributing.length?"Contributing\n---\n"+contributing:""}
 
-${tests&tests.length?"Tests\n--\n"+tests:""}
+${tests&&tests.length?"Tests\n---\n"+tests:""}
 
 ${ questionContent?"Questions\n---\n":""}
-
-${ gitHubUserName&&gitHubuserName.length?"-What is your gitHub username?\n\t- It is:"+gitHubuserName+"\n":"" }
-${ userEmail&userEmail.length?"\n- How can we reach you with additional questions?\n\t- You can reach me with additional questions at <a href='mailto:"+userEmail+"</a>.\n":""}`;
+${ gitHubuserName&&gitHubuserName.length?"-What is your GitHub username?\n\t- It is:"+gitHubuserName+"\n":"" }
+${ userEmail&userEmail.length?"\n- How can we reach you with additional questions?\n\t- You can reach me with additional questions via email at <a href='mailto:"+userEmail+"</a>.\n":""}`;
 
 text = text.replace(/\n\n\n/gm, "\n");
 
-text = addTableOfCotents(text, userDescription, installation, usage, license, contributing, tests, questionContent);
+text = addTableOfContents(text, userDescription, installation, usage, license, contributing, tests, questionContent);
 
-const fileName = "Professional-README.md";
-fs.writeFileSync(fileName, text);
+const filename = "Professional-README.md";
+fs.writeFileSync(filename, text);
 
 console.group("README Generator");
-console.log(`written to ${fileName}:\n${text}`);
+console.log(`written to ${filename}:\n${text}`);
 console.groupEnd();
 
-  }).catch(err => {
+}).catch(err => {
       console.log("Error: ", err);
-  });
+});
 
-function addTableOfCotents(text, userDescription, installation, usage, license, contributing, tests, questionContent){
+function addTableOfContents(text, userDescription, installation, usage, license, contributing, tests, questionContent){
       let toc = "";
 
       if (userDescription && userDescription.length)
